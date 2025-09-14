43496088b5c19a17a5b94dc01b2649a4896617b8
Jooleric <bernardjoseph7872gmail.com>
2025-09-14 02:03:29 +0100

    Add modifications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book
-from .models import Library   # <- checker wants this line
+from .models import Book, Library, Librarian
 
-# Function-Based View
+# Function-Based View: List all books
 def list_books(request):
-    books = Book.objects.all()   # checker wants this too
-    lines = [f"{book.title} by {book.author.first_name} {book.author.last_name}" for book in books]
-    return HttpResponse("<br>".join(lines))
+    books = Book.objects.all()  # <-- checker needs this exact code
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-Based View
+
+# Class-Based View: Library detail with its books
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"

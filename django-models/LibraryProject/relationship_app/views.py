9cb5ab004e2d272344efb13dd7162cacda5aaf61
Jooleric <bernardjoseph7872gmail.com>
2025-09-20 03:27:38 +0100

    Initial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.views.generic.detail import DetailView
-from .models import Library, Book  # Library + Book for list_books
+from .forms import RegisterForm
+from .models import Library, Book
 
 # Function-Based View: List all books
 def list_books(request):

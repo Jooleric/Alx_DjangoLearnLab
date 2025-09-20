5fb6a6b7931b5b2b15154523357e6d3af5e7d117
Jooleric <bernardjoseph7872gmail.com>
2025-09-20 03:05:22 +0100

    Add Registration form

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
+from .views import list_books, LibraryDetailView, register, login_view, logout_view
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+    path('', list_books, name='home'),  # or your homepage view
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
+    # Authentication
+    path('register/', register, name='register'),  # 👈 fix here
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm  # UserCreationForm
 from django.views.generic.detail import DetailView
 from .forms import RegisterForm
@@ -22,13 +22,13 @@ class LibraryDetailView(DetailView):
         return context
 
 # Register View
-def register_view(request):
+def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # auto login after register
-            return redirect('/')  # or redirect to dashboard
+            return redirect('/')  # or redirect to home
     else:
         form = RegisterForm()
     return render(request, "relationship_app/register.html", {"form": form})

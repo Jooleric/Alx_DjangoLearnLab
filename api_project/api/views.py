e0708a39d65ad42ae2ef7cdbc1468d10b4c8e631
Jooleric <bernardjoseph7872gmail.com>
2025-09-28 16:29:19 +0100

    fix: Switch from ViewSets to Routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+# Old ListAPIView
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New CRUD ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

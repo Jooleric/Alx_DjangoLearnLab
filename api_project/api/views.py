cb90fc7d1a7bab724424d2d5d005a266a38abdf7
Jooleric <bernardjoseph7872gmail.com>
2025-09-28 18:50:50 +0100

    Initial commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,17 +1,15 @@
-from rest_framewo import generics.ListAPIViews
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-
-# ListAPIView
-class BookList(generocs.ListAPIView):
+# ✅ ListAPIView
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # anyone can view
 
-
-# CRUD ViewSet
+# ✅ CRUD ViewSet
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # must login or use token
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 👇 This is important
     path('api/', include('api.urls')),
+    path('api/token/', obtain_auth_token),
 ]
